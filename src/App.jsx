a02d023c562d39df82e15000ee7017c28c1031a0
Faixan6190 <faixan6190@gmail.com>
2024-03-17 01:05:44 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { Delete, Send } from "@mui/icons-material";
 import "./App.css";
-import { Button } from "@mui/material";
-import Navbar from "./components/Navbar/Navbar";
+import Login from "./pages/Login";
+// import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <h1>Hello World</h1>
-      <Button variant="contained" color="error" sx={{
-        marginRight: "5px",
-      }}>
-        Sign Up
-      </Button>
-      <Button variant="outlined" endIcon={<Delete />}>
-        Delete
-      </Button>
-      <Button variant="text" endIcon={<Send />}>
-        Send
-      </Button>
+      {/* <Navbar /> */}
+      <Login />
     </>
   );
 }
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from "@mui/material"
+
+const Login = () => {
+    return (
+        <Box component={"h1"}>Login</Box>
+    )
+}
+
+export default Login;
