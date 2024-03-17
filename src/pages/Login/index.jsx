1034b94fca4046a1ecea38d7469ae5be7d2ac563
Faixan6190 <faixan6190@gmail.com>
2024-03-17 22:30:24 +0500

    update

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Box } from "@mui/material"
+import React from 'react';
+import styles from "../Login/style.module.css"
+import { Box, Divider, TextField } from "@mui/material";
+
 
 const Login = () => {
     return (
-        <Box component={"h1"}>Login</Box>
+        <Box className={styles.loginWrapper}>
+            <Box className={styles.loginForm}>
+                <h1>LOGIN</h1>
+                <Divider />
+                <Box component={"form"} mt={5}>
+                    <TextField label="Email Address" fullWidth variant="outlined" className={styles.inputField} />
+                    <TextField label="Password" fullWidth variant="outlined" />
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
-export default Login;
+export default Login
+
