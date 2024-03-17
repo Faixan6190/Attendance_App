dbb8b5fb47b3497ab218f6125c1b3f2ba122a68e
Faixan6190 <faixan6190@gmail.com>
2024-03-18 01:46:28 +0500

    Login Complete

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "../Login/style.module.css"
-import { Box, Divider, TextField } from "@mui/material";
+import { Box, Divider, TextField, Typography, Button, InputAdornment } from "@mui/material";
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 
 const Login = () => {
+
+    const [passwordShow, setPasswordShow] = useState(false)
+    const loginHandler = () => {
+        console.log("loginHandler")
+    }
+
     return (
         <Box className={styles.loginWrapper}>
             <Box className={styles.loginForm}>
                 <h1>LOGIN</h1>
                 <Divider />
-                <Box component={"form"} mt={5}>
+                <Box component={"form"} mt={"80px"} onSubmit={loginHandler}>
                     <TextField label="Email Address" fullWidth variant="outlined" className={styles.inputField} />
-                    <TextField label="Password" fullWidth variant="outlined" />
+                    <TextField type={passwordShow ? "text" : "password"} label="Password" fullWidth variant="outlined" helperText={<Typography textAlign={"right"} sx={{ cursor: "pointer" }}>Forget Password</Typography>} InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end" onClick={() => setPasswordShow(!passwordShow)} >
+                                {!passwordShow ? <VisibilityOff /> : <Visibility />}
+                            </InputAdornment>
+                        )
+                    }} />
+                    <Button variant='contained' fullWidth sx={{ mt: "20px" }} type='submit'>LOGIN</Button>
                 </Box>
             </Box>
         </Box>
