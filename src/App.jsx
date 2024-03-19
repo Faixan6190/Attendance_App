cac0f80a14642be13de5e484a38e63b4436ca2da
Faixan6190 <faixan6190@gmail.com>
2024-03-19 16:00:11 +0500

    some-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Sign Up";
+import { Routes, Route } from "react-router-dom"
 // import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <Login /> */}
-      <Signup />
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="testing" element={<Signup />} />
+        {/* <Navbar /> */}
+      </Routes>
     </>
   );
 }
