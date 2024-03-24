3be83f0033ffca93656da954c0000dca518c1dba
Faixan6190 <faixan6190@gmail.com>
2024-03-25 00:34:42 +0500

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAQWhuAh6X_4TGYny37HTdx-p00vIrK5Ms",
+//   authDomain: "attendance-app-ca17b.firebaseapp.com",
+//   projectId: "attendance-app-ca17b",
+//   storageBucket: "attendance-app-ca17b.appspot.com",
+//   messagingSenderId: "766405620025",
+//   appId: "1:766405620025:web:2a10b712ac109d2cc538e8",
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyAQWhuAh6X_4TGYny37HTdx-p00vIrK5Ms",
   authDomain: "attendance-app-ca17b.firebaseapp.com",
+  databaseURL: "https://attendance-app-ca17b-default-rtdb.firebaseio.com",
   projectId: "attendance-app-ca17b",
   storageBucket: "attendance-app-ca17b.appspot.com",
   messagingSenderId: "766405620025",
@@ -15,6 +25,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
-// const db = getFirestore(app);
+const database = getDatabase();
 
 export { app, auth, db };
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,22 +1,36 @@
 import { addDoc, collection } from 'firebase/firestore'
 import React, { useState } from 'react'
 import { db } from '../../firebase'
+import { getDatabase, ref, set } from 'firebase/database'
 
 const Dashboard = () => {
 
     const [todoValue, setTodoValue] = useState("")
-    const addTodo = async () => {
-        console.log("addTodo", todoValue)
-        try {
-            const docRef = await addDoc(collection(db, "todos"), {
-                value: todoValue
-            });
-            console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
+    const [valueTodo, setValueTodo] = useState("")
+    // const addTodo = async () => {
+    //     try {
+    //         const docRef = await addDoc(collection(db, "todos"), {
+    //             value: todoValue
+    //         });
+    //         console.log("Document written with ID: ", docRef.id);
+    //     } catch (e) {
+    //         console.error("Error adding document: ", e);
+    //     }
+    // }
 
+    function writeUserData() {
+        const db = getDatabase();
+        set(ref(db, 'todos'), {
+            value: todoValue,
+        });
     }
+    function writeUserData1() {
+        const db = getDatabase();
+        set(ref(db, 'testing'), {
+            testing: valueTodo,
+        });
+    }
+
 
     return (
         <>
@@ -27,7 +41,9 @@ const Dashboard = () => {
                 marginTop: "20px"
             }}>
                 <input onChange={(e) => setTodoValue(e.target.value)} type="text" placeholder='Enter Todo' />
-                <button onClick={addTodo}>ADD</button>
+                <button onClick={writeUserData}>ADD</button>
+                <input onChange={(e) => setValueTodo(e.target.value)} type="text" placeholder='Testing' />
+                <button onClick={writeUserData1}>ADD</button>
             </div>
         </>
     )
