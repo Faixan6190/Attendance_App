b94e015642f88a1df8a851cdd56608e6c8567012
Faixan6190 <faixan6190@gmail.com>
2024-04-10 00:47:42 +0500

    update dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,13 @@
 import { addDoc, collection } from 'firebase/firestore'
 import React, { useState } from 'react'
-import { db } from '../../firebase'
+import { auth, db } from '../../firebase'
 import { AdminLayout, InputField } from '../../components'
 import { Button, Divider, Box, Grid, Container } from '@mui/material'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { styled } from '@mui/material/styles';
-
+import "./dashboard.css"
+import { ToastAlert } from "../../utils/toast"
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 
 const Dashboard = () => {
@@ -36,6 +38,22 @@ const Dashboard = () => {
 
     const [fullName, setFullName] = useState("")
     const [course, setCourse] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleAddStd = async () => {
+        try {
+            // console.log("handleAddStd", fullName, course, email, password)
+            if (!fullName || !course || !email || !password) {
+                ToastAlert("required field are missing", "error")
+                return
+            }
+            const stdData = await createUserWithEmailAndPassword(auth, email, password)
+            console.log(stdData, "stdData")
+        } catch (error) {
+            ToastAlert(error.code || error.message, "error")
+        }
+    }
 
     return (
         <>
@@ -54,16 +72,16 @@ const Dashboard = () => {
                 <Container>
                     <Grid container mt={2} columnSpacing={5} rowSpacing={3}>
                         <Grid item sm={6}>
-                            <InputField id="fullName" label="Full Name" onChange={(e) => console.log(e.target.value)} />
+                            <InputField id="fullName" label="Full Name" onChange={(e) => setFullName(e.target.value)} />
                         </Grid>
                         <Grid item sm={6}>
-                            <InputField id="course" label="Course" />
+                            <InputField id="course" label="Course" onChange={(e) => setCourse(e.target.value)} />
                         </Grid>
                         <Grid item sm={6}>
-                            <InputField id="email" label="Email" />
+                            <InputField id="email" label="Email" onChange={(e) => setEmail(e.target.value)} />
                         </Grid>
                         <Grid item sm={6}>
-                            <InputField id="Password" label="Password" type="password" />
+                            <InputField id="Password" label="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
                         </Grid>
                         <Grid item sm={12}>
                             {/* <input type="file" /> */}
@@ -79,7 +97,8 @@ const Dashboard = () => {
                             </Button>
                         </Grid>
                         <Grid item sm={12}>
-                            <Button variant="contained" sx={{ width: "100%" }}>Add Student</Button>
+                            <button onClick={handleAddStd} className="button" style={{ verticalAlign: "middle" }}><span>Add Student</span></button>
+                            {/* <Button variant="contained" sx={{ width: "100%" }}>Add Student</Button> */}
                         </Grid>
                     </Grid>
                 </Container>
