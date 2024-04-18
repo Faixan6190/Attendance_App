b81c157b39f78e61cd483b1ba607012a9f0fff9a
Faixan6190 <faixan6190@gmail.com>
2024-04-18 14:47:01 +0500

    adding

--- src/pages/StdList/index.jsx
+++ src/pages/StdList/index.jsx
@@ -5,7 +5,7 @@ import { Divider } from '@mui/material'
 const StdList = () => {
     return (
         <AdminLayout>
-            <h1>Student</h1>
+            <h1>Students</h1>
             <Divider />
         </AdminLayout >
     )
