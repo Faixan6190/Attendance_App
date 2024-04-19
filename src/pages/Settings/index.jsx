54d0df8347c431cc8e3414f91d005ec8b9c42710
Faixan6190 <faixan6190@gmail.com>
2024-04-20 00:36:52 +0500

    'update'

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,25 +1,53 @@
 import { doc, getDoc } from 'firebase/firestore'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { db } from "../../firebase"
+import { Button, Grid } from '@mui/material'
+import { InputField } from "../../components"
 
 const Settings = () => {
+    const [fullName, setFullName] = useState("")
+    const [course, setCourse] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [stdimage, setstdimage] = useState("")
+    const [isLoading, setIsLoading] = useState(false);
+    const [userData, setuserdata] = useState("")
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
                 const UID = localStorage.getItem("uid")
                 const user = await getDoc(doc(db, "users", UID))
-                console.log(user, "user")
-
+                setuserdata(user.data())
             } catch (error) {
                 ToastAlert(error.code || error.message, "error")
             }
         }
         fetchUser()
     }, [])
+    console.log(userData, "userData")
 
     return (
-        <div> Settings</div >
+        <Grid container mt={2} columnSpacing={5} rowSpacing={3}>
+            <Grid item sm={6}>
+                <InputField id="fullName" label="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+            </Grid>
+            <Grid item sm={6}>
+                <InputField id="course" label="Course" value={course} onChange={(e) => setCourse(e.target.value)} />
+            </Grid>
+            <Grid item sm={6}>
+                <InputField id="email" label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </Grid>
+            <Grid item sm={6}>
+                <InputField id="Password" value={password} label="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+            </Grid>
+            <Grid item sm={12}>
+                {/* <button onClick={handleAddStd} disabled={isLoading} className="button" style={{ verticalAlign: "middle" }}>   {isLoading ? 'Loading...' : ''}<span>Add Student</span></button> */}
+                <button className="button" style={{ verticalAlign: "middle" }}>
+                    {isLoading ? <span><Loader /></span> : <span>Add Student</span>}
+                </button>
+            </Grid>
+        </Grid>
     )
 }
 
