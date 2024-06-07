3bbaa291fa47e057b1a264e117698a0cd05bfa56
Faixan6190 <faixan6190@gmail.com>
2024-06-07 19:50:18 +0500

    change name

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -64,8 +64,7 @@ const Dashboard = () => {
   return (
     <>
       <AdminLayout>
-        <h1>Add Student</h1>
-
+        <h1>Add Students</h1>
         <Divider />
         <Container>
           <Grid container mt={2} columnSpacing={5} rowSpacing={5}>
