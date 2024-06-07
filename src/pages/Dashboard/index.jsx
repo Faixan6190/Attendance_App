0ff272b2665267e723f36185b5b27f30fa71667a
Faixan6190 <faixan6190@gmail.com>
2024-06-07 19:50:41 +0500

    fix name'

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -64,7 +64,7 @@ const Dashboard = () => {
   return (
     <>
       <AdminLayout>
-        <h1>Add Students</h1>
+        <h1>Add Student</h1>
         <Divider />
         <Container>
           <Grid container mt={2} columnSpacing={5} rowSpacing={5}>
